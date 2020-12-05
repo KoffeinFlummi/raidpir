002773929d2dcbe1c2ef14b2a152012b0beca520
KoffeinFlummi <koffeinflummi@protonmail.com>
2020-12-05 20:55:25 +0100

    Switch to Criterion for benchmarks

--- benches/pir.rs
+++ benches/pir.rs
@@ -1,32 +1,73 @@
-#![feature(test)]
-
-extern crate test;
-use test::Bencher;
+use criterion::*;
 
 use rand::rngs::StdRng;
 use rand::{RngCore, SeedableRng};
 
 use raidpir::client::RaidPirClient;
 use raidpir::server::RaidPirServer;
+use raidpir::util::*;
 
-#[bench]
-fn bench_query(b: &mut Bencher) {
-    let mut prng = StdRng::from_entropy();
+fn bench_query(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Query");
+    group.plot_config(PlotConfiguration::default()
+        .summary_scale(AxisScale::Logarithmic));
 
-    let mut db: Vec<u32> = Vec::with_capacity(1 << 22);
-    for _i in 0..(1 << 22) {
-        db.push(prng.next_u32());
+    for exp in [14, 16, 18, 20, 22].iter() {
+        let size = 1usize << exp;
+
+        for threads in [1, 2, 4].iter() {
+            group.bench_with_input(BenchmarkId::new(format!("t={}", threads), size), &size, |bench, size| {
+                let threadpool = rayon::ThreadPoolBuilder::new().num_threads(*threads).build().unwrap();
+                threadpool.install(|| {
+                    let mut prng = StdRng::from_entropy();
+
+                    let mut db: Vec<u8> = vec![0; *size];
+                    prng.fill_bytes(&mut db);
+
+                    let mut servers: Vec<RaidPirServer<u8>> = (0..2)
+                        .map(|i| RaidPirServer::new(db.clone(), i, 2, 2))
+                        .collect();
+
+                    let client = RaidPirClient::new(db.len(), 2, 2);
+
+                    let seeds = servers.iter_mut().map(|s| s.seed()).collect();
+
+                    bench.iter(|| {
+                        client.query(42, &seeds);
+                    });
+                });
+            });
+        }
     }
-
-    let mut servers: Vec<RaidPirServer<u32>> = (0..4)
-        .map(|i| RaidPirServer::new(db.clone(), i, 4, 3))
-        .collect();
-
-    let client = RaidPirClient::new(db.len(), 4, 3);
-
-    let seeds = servers.iter_mut().map(|s| s.seed()).collect();
-
-    b.iter(|| {
-        client.query(42, &seeds);
-    });
 }
+
+fn bench_xoring(c: &mut Criterion) {
+    let mut group = c.benchmark_group("XOR");
+    group.plot_config(PlotConfiguration::default()
+        .summary_scale(AxisScale::Logarithmic));
+
+    for exp in [14, 16, 18, 20, 22].iter() {
+        let size = 1usize << exp;
+
+        for threads in [1, 2, 4].iter() {
+            group.bench_with_input(BenchmarkId::new(format!("t={}", threads), size), &size, |bench, size| {
+                let threadpool = rayon::ThreadPoolBuilder::new().num_threads(*threads).build().unwrap();
+                threadpool.install(|| {
+                    let mut prng = StdRng::from_entropy();
+
+                    let mut a: Vec<usize> = Vec::with_capacity(*size);
+                    for _i in 0..*size {
+                        a.push(prng.next_u64() as usize);
+                    }
+
+                    let b: Vec<usize> = vec![42; *size];
+
+                    bench.iter(|| xor_into_slice(&mut a, &b));
+                });
+            });
+        }
+    }
+}
+
+criterion_group!(benches, bench_query, bench_xoring);
+criterion_main!(benches);
